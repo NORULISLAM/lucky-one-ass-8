9eb1d4febcb4e3c34f33c281bdc32d5fca6bca9d
user <juyelr16@gmail.com>
2022-03-26 07:19:47 +0900

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='book'>
+            <h1>Famous Bookstores</h1>
+            <h3>Choose Your Own Adventure Books</h3>
+        </div>
+    );
+};
+
+export default Header;
