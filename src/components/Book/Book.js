aba316e64fb838b3b58bcdf809f47158783e987f
user <juyelr16@gmail.com>
2022-03-26 10:20:10 +0900

    2nd commit

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -3,14 +3,17 @@ import Product from '../Product/Product';
 import './Book.css';
 const Book = () => {
     const [products, setProducts] = useState([]);
-
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
 
     }, [])
-
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className='book-container'>
@@ -19,12 +22,15 @@ const Book = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
 
             <div className="cart-container">
                 <h3>Selected Product</h3>
+                {console.log(cart)}
+                <p>Item: {cart[0].name}</p>
 
             </div>
         </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,27 @@
+
 import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const { name, img, price } = props.product;
+
+    const { product, handleAddToCart } = props;
+    const { name, img, price, ratings } = product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price:${price}</p>
+                <p><small>Ratings:{ratings}stars</small></p>
+            </div>
+
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                <p className='btn-text'>Add to Cart</p>
+            </button>
+
         </div>
+
+
     );
 };
 
